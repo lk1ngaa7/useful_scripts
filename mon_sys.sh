34fe419f054827df0c55e7bcc3880c1f15066da4
lk1ngaa7 <lk1ngaa7@gmail.com>
2016-05-15 15:12:15 +0800

    add mon

--- mon_sys.sh
+++ mon_sys.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
-top -n 1 | grep Cpu | awk '{print $8 , $10 }' > /root/opt/data/mon/sys/cpu.txt # idle iowait
-free -m | grep Mem | awk '{print $2 , $3 , $4 }' > /root/opt/data/mon/sys/mem.txt # total used free
+for i in {1..20}
+do
+    sleep 3
+    echo $i
+    #top -n 1 | grep Cpu | awk '{print $8 , $10 }' > /root/opt/data/mon/sys/cpu.txt # idle iowait
+    vmstat | sed -n '3p' | awk '{print $15 , $16}' > /root/opt/data/mon/sys/cpu.txt # idle iowait
+    free -m | grep Mem | awk '{print $2 , $3 , $4 }' > /root/opt/data/mon/sys/mem.txt # total used free
+    /root/ifstat-1.1/ifstat  -i eth1 2 1 | sed -n '3p' | awk '{print $1,$2}' > /root/opt/data/mon/sys/net.txt
+done
